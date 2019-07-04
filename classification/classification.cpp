5be82add4e53e125770c30705d14c65fc4ef2f2f
Nesterov Alexander <nesoldr@mail.ru>
2019-07-04 10:00:18 +0300

    Update classification

--- classification/classification.cpp
+++ classification/classification.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    if (argc < 3)
+    if (argc < 4)
     {
         std::cout << "ERROR ARGS" << '\n';
         return 0;
@@ -17,17 +17,22 @@ int main(int argc, char** argv)
 
     string model  = string(argv[1]);
     string config = string(argv[2]);
+    string path_image = string(argv[3]);
     Net net = readNet(model, config);
 
-    VideoCapture cap(0);
+    Mat frame = imread(path_image);
 
-    Mat frame, blob;
-    while (waitKey(1) < 0)
-    {
-        cap >> frame;
-        blobFromImage(frame, blob, 0.017, Size(224, 224), {103.94,116.78,123.68}, swapRB, false);
-        net.setInput(blob);
-        Mat prob = net.forward();
-    }
+    Mat blob;
+    blobFromImage(frame, blob, 0.017, Size(224, 224), {103.94,116.78,123.68});
+    net.setInput(blob);
+    Mat prob = net.forward();
+
+    Point classIdPoint;
+    double confidence;
+    minMaxLoc(prob.reshape(1, 1), 0, &confidence, 0, &classIdPoint);
+    int classId = classIdPoint.x;
+    std::cout << "Class: " << classId << '\n';
+    std::cout << "Confidence: " << confidence << '\n';
+
     return 0;
 }
